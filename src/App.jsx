ab692fcc8ca69055987c3849135021f8cff8a522
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-27 20:03:33 +0530

    set up Shadcn as UI library and eslint

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import { Button } from '@/components/ui/button';
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-3xl text-blue-500 font-bold underline">
+      Hello world!
+    </h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
