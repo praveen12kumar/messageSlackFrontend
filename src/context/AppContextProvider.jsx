980516d03018f20f7986705652511e99c22b7d6c
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-30 21:32:20 +0530

    Added Socket context

--- src/context/AppContextProvider.jsx
+++ src/context/AppContextProvider.jsx
@@ -3,14 +3,17 @@ import combineContext from '@/utils/combineContext';
 import { AuthContextProvider } from './AuthContext';
 import { CreateChannelContextProvider } from './CreateChannelContext';
 import {CreateWorkspaceContextProvider} from './CreateWorkspaceContext';
+import { SocketContextProvider } from './SocketContext';
 import { WorkspaceContextProvider } from './WorkspaceContext';
 import { WorkspacePreferencesModalContextProvider } from './WorkspacePreferencesModalContext';
 
 
 export const AppContextProvider = combineContext(
+        SocketContextProvider,
         AuthContextProvider,
         CreateWorkspaceContextProvider,
         WorkspacePreferencesModalContextProvider,
         CreateChannelContextProvider,
-        WorkspaceContextProvider
+        WorkspaceContextProvider,
+        
     );
